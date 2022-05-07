ce32c6e921fbcc26e760d2a55fd0b41dfcad9ed4
gellwanger <gustavojce@gmail.com>
2022-05-07 15:33:31 -0300

    Cria interfaces em TS

--- Javascript/Typescript/index.ts
+++ Javascript/Typescript/index.ts
@@ -3,3 +3,26 @@ function soma(a: number, b: number) {
 }
 
 console.log(soma(3, 1));
+
+interface IAnimal {
+  nome: string;
+  tipo: 'terrestre' | 'aquático';
+  executarRugido(altudaEmDecibeis: number): void;
+}
+
+interface IFelino extends IAnimal {
+  visaoNoturna: boolean;
+}
+
+const animal: IAnimal = {
+  nome: 'Elefante',
+  tipo: 'terrestre',
+  executarRugido: (altudaEmDecibeis) => (`${ altudaEmDecibeis }dB`),
+}
+
+const felino: IFelino = {
+  nome: 'Leão',
+  tipo: 'terrestre',
+  visaoNoturna: true,
+  executarRugido: (altudaEmDecibeis) => (`${ altudaEmDecibeis }dB`),
+}
