431fed61c67330c3075485afabb47130e96b22a1
gellwanger <gustavojce@gmail.com>
2022-05-07 16:23:18 -0300

    Adiciona input a pagina HTML com TS

--- Javascript/Typescript/index.ts
+++ Javascript/Typescript/index.ts
@@ -1,41 +1,48 @@
-function soma(a: number, b: number) {
-  return a + b;
-}
+// function soma(a: number, b: number) {
+//   return a + b;
+// }
 
-console.log(soma(3, 1));
+// console.log(soma(3, 1));
 
-interface IAnimal {
-  nome: string;
-  tipo: 'terrestre' | 'aquático';
-  domestico: boolean;
-}
+// interface IAnimal {
+//   nome: string;
+//   tipo: 'terrestre' | 'aquático';
+//   domestico: boolean;
+// }
 
-interface IFelino extends IAnimal {
-  visaoNoturna: boolean;
-}
+// interface IFelino extends IAnimal {
+//   visaoNoturna: boolean;
+// }
 
-interface ICanino extends IAnimal {
-  porte: 'pequeno' | 'médio' | 'grande';
-}
+// interface ICanino extends IAnimal {
+//   porte: 'pequeno' | 'médio' | 'grande';
+// }
 
-type IDomestico = IFelino | ICanino;
+// type IDomestico = IFelino | ICanino;
 
-const pet: IDomestico = {
-  domestico: true,
-  nome: 'Cacau',
-  porte: 'pequeno',
-  tipo: 'terrestre',
-}
+// const pet: IDomestico = {
+//   domestico: true,
+//   nome: 'Cacau',
+//   porte: 'pequeno',
+//   tipo: 'terrestre',
+// }
 
-const animal: IAnimal = {
-  nome: 'Elefante',
-  tipo: 'terrestre',
-  domestico: false,
-}
+// const animal: IAnimal = {
+//   nome: 'Elefante',
+//   tipo: 'terrestre',
+//   domestico: false,
+// }
 
-const felino: IFelino = {
-  nome: 'Leão',
-  tipo: 'terrestre',
-  visaoNoturna: true,
-  domestico: false,
-}
+// const felino: IFelino = {
+//   nome: 'Leão',
+//   tipo: 'terrestre',
+//   visaoNoturna: true,
+//   domestico: false,
+// }
+
+const input = document.getElementById('input') as HTMLInputElement;
+
+input.addEventListener('input', (event) => {
+  const i = event.currentTarget as HTMLInputElement; 
+  console.log(i.value);
+});
