f032f92db83996613eade9ddbe13e9724b502b41
gellwanger <gustavojce@gmail.com>
2022-05-08 00:38:05 -0300

    Adiciona o botão de remover

--- Sistema de Estacionamento em TypeScript/script.ts
+++ Sistema de Estacionamento em TypeScript/script.ts
@@ -1,12 +1,19 @@
 interface Veiculo {
   nome: string;
   placa: string;
-  entrada: Date;
+  entrada: Date | string;
 }
 
 (function () {
   const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
   
+  function calcTempo(mil: number) {
+    const min = Math.floor(mil / 60000);
+    const sec = Math.floor((mil % 60000) / 1000);
+
+    return `${min}m e ${sec}s`;
+  }
+
   function patio() {
     function ler(): Veiculo[] {
       return localStorage.patio ? JSON.parse(localStorage.patio) : []
@@ -33,12 +40,27 @@ interface Veiculo {
           </td>
         `;
 
+        row.querySelector(".delete")?.addEventListener("click", function() {
+          remover(this.dataset.placa)
+        })
+
         $("#patio")?.appendChild(row);
 
         if (foiSalvo) salvar([...ler(), veiculo])
     }
 
-    function remover() {}
+    function remover(placa: string) {
+      const { entrada, nome } = ler().find(veiculo => veiculo.placa === placa);
+      const tempo = calcTempo(new Date().getTime() - new Date(entrada).getTime());
+
+      if (
+        !confirm(`O veículo ${ nome } permaneceu por ${ tempo }. Deseja encerrar? `)
+      ) 
+        return;
+
+      salvar(ler().filter(veiculo => veiculo.placa !== placa));
+      render();
+    }
 
     function render() {
       $("#patio")!.innerHTML = "";
@@ -64,6 +86,6 @@ interface Veiculo {
     return;
   }
 
-  patio().adicionar({ nome, placa, entrada: new Date() }, true);
+  patio().adicionar({ nome, placa, entrada: new Date().toISOString() }, true);
   });
 })();
